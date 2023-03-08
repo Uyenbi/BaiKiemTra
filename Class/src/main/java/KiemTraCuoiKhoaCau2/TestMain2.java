707b2d9f04ff5e897dcdee1800c2921a09206605
Uyenbi <127296851+Uyenbi@users.noreply.github.com>
2023-03-08 16:30:55 +0700

    Add files via upload

---
+++ Class/src/main/java/KiemTraCuoiKhoaCau2/TestMain2.java
@@ -0,0 +1,29 @@
+package KiemTraCuoiKhoaCau2;
+
+import java.util.Scanner;
+
+public class TestMain2 {
+    public static void main(String[] args) {
+        NhanVien kt1 = new KyThuat("KT01","Nguyen Van A", "01/02/2000","HN", "CNTT");
+        System.out.println(kt1);
+        System.out.println(kt1.moTaCongViec());
+
+        // Input
+        Scanner in = new Scanner(System.in);
+        System.out.print("Nhập mã: ");
+        String maNhanVien = in.nextLine();
+        System.out.print("Nhập họ tên: ");
+        String hoTen = in.nextLine();
+        System.out.print("Nhập năm sinh: ");
+        String namSinh = in.nextLine();
+        System.out.print("Nhập địa chỉ: ");
+        String diaChi = in.nextLine();
+        System.out.print("Nhập chuyên nghành: ");
+        String chuyenNghanh = in.nextLine();
+
+        NhanVien kt2 = new KyThuat(maNhanVien,hoTen, namSinh, diaChi, chuyenNghanh);
+        System.out.println(kt2);
+        System.out.println(kt2.moTaCongViec());
+
+    }
+}
---
+++ Class/src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+        public static void main(String[] args) {
+            // Dog
+            Dog d1 = new Dog();
+            d1.makeSound();
+            // Animal - tao thu
+//        Animal a = new Animal();
+        }
+    }
